880bffa96f340b70ecce1d06675978d18d8945f2
Andela Developer <johnboscoohia@gmail.com>
2018-06-08 13:14:36 +0100

    fix bug on signup view

--- server/dev_team_app/settings.py
+++ server/dev_team_app/settings.py
@@ -66,7 +66,7 @@ JWT_AUTH = {
     'JWT_RESPONSE_PAYLOAD_HANDLER':
     'rest_framework_jwt.utils.jwt_response_payload_handler',
 
-    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_SECRET_KEY': 'whosisherecoming',
     'JWT_GET_USER_SECRET_KEY': None,
     'JWT_PUBLIC_KEY': None,
     'JWT_PRIVATE_KEY': None,
