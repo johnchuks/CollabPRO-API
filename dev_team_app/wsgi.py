93d88e94fe55d02a7e800671cc9d8f32e3fc7f3d
Andela Developer <johnboscoohia@gmail.com>
2018-06-12 18:42:39 +0100

    remove unnecessary comments

--- dev_team_app/wsgi.py
+++ dev_team_app/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-print('we global now nnnnnnnnnnnn')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dev_team_app.settings")
 
 application = get_wsgi_application()
